8bfc5deb10fcc71bcaf444631b685a05b63b50d0
Pedro Rodriguez <78929255+PedroLuiss@users.noreply.github.com>
2022-06-28 16:42:13 -0400

    Cambiar query de index estudent

--- app/Http/Controllers/EstudiantesController.php
+++ app/Http/Controllers/EstudiantesController.php
@@ -26,8 +26,12 @@ class EstudiantesController extends Controller
      */
     public function index()
     {
-        $estu = Estudiantes::select('*')->join('carreras', 'carreras.id', '=', 'estudiantes.carreras_id')->get();
-
+        $estu = Estudiantes::select('estudiantes.id','estudiantes.cedula','estudiantes.nombres','estudiantes.primer_apellido',
+        'estudiantes.segundo_apellido','carreras.name','estudiantes.fe_ingreso','estudiantes.inicio_programa','estudiantes.sexo','estudiantes.sanguineo',
+        'estudiantes.edo_civil','estudiantes.condicion','estudiantes.nucleo','estudiantes.etnia','estudiantes.discapacidad','estudiantes.pais','estudiantes.fe_nac',
+        'estudiantes.lugar_nac','estudiantes.ciudad','estudiantes.direccion','estudiantes.tel_hab','estudiantes.tel_cel','estudiantes.email')
+        ->join('carreras', 'carreras.id', '=', 'estudiantes.carreras_id')->get();
+        // return response($estu);
         return view('estudiantedatos.index', compact('estu'));
     }
 
