82e45e816e84362cd7df99e22c9e6cc3ca926134
Yixon Smith <yixon2011@gmail.com>
2023-04-30 23:47:18 -0400

    cambio jon importar excel

---
+++ resources/views/estudiantedatos/add_secretaria_import.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('content')
+    @if (session('mensaje'))
+        <div class="alert alert-success">
+            <strong>{{ session('mensaje') }}</strong>
+        </div>
+    @endif
+
+    @can('isAdmin')
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header border-0 pt-5">
+                            <!--begin::Title-->
+                            <h3 class="card-title align-items-start flex-column">
+                                <span class="card-label fw-bold fs-2 h1 mb-1">Importar Estudiantes</span>
+
+                                <span class="text-muted fw-semibold fs-7">Asegurese que el archivo sea formato Excel</span>
+                            </h3>
+                            <!--end::Title-->
+                        </div>
+
+                        <div class="card-body">
+                            @if (isset($errors) && $errors->any())
+                                <div class="alert alert-danger" role="alert">
+                                    @foreach ($errors->all() as $error)
+                                        {{ $error }}
+                                    @endforeach
+                                </div>
+                            @endif
+
+                            <form action="{{ route('estudiantedatos.importdocexcel') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="input-group mb-3">
+                                    <input type="file" class="form-control h-100" id="import_file" name="exceldocumento">
+                                  
+                                    <button class="btn btn-primary btn-sm" id="btn_import_exel_student" type="submit">Importar</button>
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @push('scripts')
+            <script>
+                var hostUrl = "/m2/assets/";
+            </script>
+            <script src="/m2/assets/plugins/global/plugins.bundle.js"></script>
+            <script src="/m2/assets/js/scripts.bundle.js"></script>
+            <!--end::Global Javascript Bundle-->
+            <!--begin::Page Vendors Javascript(used by this page)-->
+            <script src="/m2/assets/plugins/custom/datatables/datatables.bundle.js"></script>
+            <!--end::Page Vendors Javascript-->
+            <!--begin::Page Custom Javascript(used by this page)-->
+            <script src="/m2/assets/js/custom/apps/user-management/users/list/table.js"></script>
+            {{-- <script src="https://preview.keenthemes.com/metronic8/demo1/assets/plugins/custom/datatables/datatables.bundle.js"></script> --}}
+            <script src="/m2/assets/js/custom/apps/user-management/users/list/add.js"></script>
+            <script src="/m2/assets/js/widgets.bundle.js"></script>
+            <script src="/m2/assets/js/custom/widgets.js"></script>
+            <script src="/m2/assets/js/custom/apps/chat/chat.js"></script>
+            <script src="/m2/assets/js/custom/intro.js"></script>
+            <script>
+                $('#btn_import_exel_student').on('click',(e)=>{
+                    $('.loader').show();
+                    console.log("Hola");
+                });
+            </script>
+        @endpush
+    @endcan
+@endsection
--- resources/views/estudiantedatos/index.blade.php
+++ resources/views/estudiantedatos/index.blade.php
@@ -43,11 +43,21 @@
                     <!--end::Svg Icon--></svg></span>Registro
                 </a>
 
-                <form action="{{route('estudiantedatos.importdocexcel')}}" class="btn btn-sm btn-primary" method="POST" enctype="multipart/form-data" >
-                    @csrf
-                    <input type="file"  name="exceldocumento" >
-                    <button type="submit" class="btn-primary">Import</button>
-                </form>
+                <!--begin::Add user-->
+                    <a href="{{route('estudiantedatos.student_ssc_secretaria')}}" class=" ms-5 btn btn-primary">
+                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                        <span class="svg-icon svg-icon-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2"
+                                    rx="1" transform="rotate(-90 11.364 20.364)" fill="currentColor"></rect>
+                                <rect x="4.36396" y="11.364" width="16" height="2" rx="1"
+                                    fill="currentColor"></rect>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->Importar Excel
+                    </a>
+                    <!--end::Add user-->
 
             </div>
 
